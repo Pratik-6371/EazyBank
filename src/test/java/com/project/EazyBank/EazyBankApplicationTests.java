1b9e29430e4b88d3ceb28497d9b7b31fe30dc4d1
Pratik-6371 <luckypratikjena00@gmail.com>
2023-11-30 13:03:13 +0530

    initial commit

---
+++ src/test/java/com/project/EazyBank/EazyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.EazyBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
